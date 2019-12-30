72af59251e87a4180090f6e8adda559fe3ee355a
po_chih <up46qs85d3y@gmail.com>
2019-12-30 19:56:45 +0800

    cal score with ord_num

--- source/algorithm_A.cpp
+++ source/algorithm_A.cpp
@@ -26,91 +26,38 @@ using namespace std;
 *************************************************************************/
 #define rows 5
 #define cols 6
-
-bool isCorner(int row,int col){
-    if((row==0 && col==0) || (row==0 && col==cols-1) || (row==rows-1 && col==0) || (row==rows-1 && col == cols-1))
-        return true;
-    return false;
-}
-int enemy_move(Board enemy_board,int row,int col,Player *player_ptr,Player *enemy_ptr,int color,int enemy_color){
-    enemy_board.place_orb(row,col,enemy_ptr);
-    //enemy_board.print_current_board(row,col,100000000);
-    int new_ord=0;
+int enemy_move(Board enemy_board,int row,int col,Player* player,Player* enemy){
+    enemy_board.place_orb(row,col,enemy);
+    int remained_ord=0;
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            if(enemy_board.get_cell_color(i,j)==color){
-                new_ord++;
+            if(enemy_board.get_cell_color(i,j)==player->get_color()){
+                remained_ord+=enemy_board.get_orbs_num(i,j);
             }
         }
     }
-    return new_ord;
+    return remained_ord;
 }
-int you_move(Board moved_board,int row,int col,Player *player_ptr,Player *enemy_ptr,int init_ord,int color,int enemy_color){
-    int bonus=0;
-    int min_score=10000;
-    //cal bonus
-    //isCorner
-    if(isCorner(row,col) && moved_board.get_orbs_num(row,col)==0)
-        bonus+=1;
-    //enemy ord surrounding
-    if(row-1>=0 && moved_board.get_cell_color(row-1,col)==enemy_color && moved_board.get_capacity(row-1,col)-moved_board.get_orbs_num(row-1,col)==moved_board.get_capacity(row,col)-moved_board.get_orbs_num(row,col)){
-        bonus+=1;
-    }
-    if(row+1<rows && moved_board.get_cell_color(row+1,col)==enemy_color && moved_board.get_capacity(row+1,col)-moved_board.get_orbs_num(row+1,col)==moved_board.get_capacity(row,col)-moved_board.get_orbs_num(row,col)){
-        bonus+=1;
-    }
-    if(col-1>=0 && moved_board.get_cell_color(row,col-1)==enemy_color && moved_board.get_capacity(row,col-1)-moved_board.get_orbs_num(row,col-1)==moved_board.get_capacity(row,col)-moved_board.get_orbs_num(row,col)){
-        bonus+=1;
-    }
-    if(col+1<cols && moved_board.get_cell_color(row,col+1)==enemy_color && moved_board.get_capacity(row,col+1)-moved_board.get_orbs_num(row,col+1)==moved_board.get_capacity(row,col)-moved_board.get_orbs_num(row,col)){
-        bonus+=1;
-    }
-    
-    
-    //cout<<"***********************************************************************"<<endl;
-    //cout<<"                      if you move to ("<<row<<","<<col<<")"<<endl;
-    moved_board.place_orb(row,col,player_ptr);
-    //moved_board.print_current_board(row,col,0);
-    //cout<<"***********************************************************************"<<endl;
-    
+int move(Board board,int row,int col,Player* player,Player* enemy,int init_ord){
+    board.place_orb(row,col,player);
+    int min=10000;
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            if(moved_board.get_cell_color(i, j) == enemy_color || moved_board.get_cell_color(i, j) == 'w'){
-                int score;
-                //cout<<"------------------------------------------"<<endl;
-                //cout<<"init_ord = "<<init_ord<<endl;
-                //cout<<"if enemy move to ("<<i<<","<<j<<")"<<endl;
-                int new_ord = enemy_move(moved_board,i,j,player_ptr,enemy_ptr,color,enemy_color);
-                //cout<<"new_ord = "<<new_ord<<endl;
-                score = new_ord - init_ord;
-                //cout<<"score: "<<score<<endl;
-                if(score<min_score){
-                    min_score=score;
+            if(board.get_cell_color(i,j) == enemy->get_color()){
+                int remained_ord=enemy_move(board,i,j,player,enemy);
+                if(remained_ord < min){
+                    min = remained_ord;
                 }
             }
         }
     }
-    
-    return min_score + bonus;
+    return min - init_ord;
 }
 void algorithm_A(Board board, Player player, int index[]){
-    
-    // cout << board.get_capacity(0, 0) << endl;
-    // cout << board.get_orbs_num(0, 0) << endl;
-    // cout << board.get_cell_color(0, 0) << endl;
-    // board.print_current_board(0, 0, 0);
 
-    //////////// Random Algorithm ////////////
-    // Here is the random algorithm for your reference, you can delete or comment it.
-    int row, col;
     int color = player.get_color();
-    int enemy_color ;
-    if(color == 'r')
-        enemy_color='b';
-    else
-        enemy_color='r';
+    int enemy_color = (color == 'r' ? 'b':'r' );
     Player enemy(enemy_color);
-
     //win
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
@@ -128,41 +75,28 @@ void algorithm_A(Board board, Player player, int index[]){
             }
         }
     }
-    //best
-    //cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-    int max_score = -10000;
-    //count ord_num
-    int init_ord = 0;
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            if(board.get_cell_color(i,j)==color)
-                init_ord++;
-        }
-    }
 
+    //count init_ord
+    int init_ord=0;
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            int score=0;
-            if(board.get_cell_color(i, j) == color || board.get_cell_color(i, j) == 'w'){
-                score=you_move(board,i,j,&player,&enemy,init_ord,color,enemy_color);
-                //cout<<endl;
-                //cout<<"The score placing ord in ("<<i<<","<<j<<") is "<<score<<endl;
-                if(score>max_score){
-                    max_score = score;
-                    index[0]=i;
-                    index[1]=j;
-                }
-                //cout<<"curren_max_score = "<<max_score<<endl;
+            if(board.get_cell_color(i,j)==color){
+                init_ord+=board.get_orbs_num(i,j);
             }
         }
     }
-
-    //cout<<endl;
-    //cout<<endl;
-    //cout<<"***********************************************************************"<<endl;
-    //cout<<"max_score = "<<max_score<<endl;
-    //cout<<"best step is ("<<index[0]<<","<<index[1]<<")"<<endl;
-    //cout<<"***********************************************************************"<<endl;
-    //cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-    
+    //choose best step
+    int max = -10000;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(board.get_cell_color(i, j) == color || board.get_cell_color(i, j) == 'w'){
+                int score=move(board,i,j,&player,&enemy,init_ord);
+                if(score > max){
+                    max = score;
+                    index[0]=i;
+                    index[1]=j;
+                }
+            }
+        }
+    }
 }
